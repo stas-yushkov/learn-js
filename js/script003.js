1781f353f167b03989386b6062fd42f6a0c6bc1e
stas-yushkov <stas.yushkov@gmail.com>
2021-04-09 01:28:34 +0300

    Константа

---
+++ js/script003.js
@@ -0,0 +1,10 @@
+"use strict"
+// Константа хранящая значение цвета
+const COLOR_TEAL = '#009688';
+
+// Константа хранящая сообщение о авторизации
+const MESSAGE_LOGIN_SUCCESS = 'Добро пожаловать!';
+
+const SOME_TEST_MESSAGE = "SOME_TEST_MESSAGEjfjfj";
+
+console.log("SOME_TEST_MESSAGE =",SOME_TEST_MESSAGE);
