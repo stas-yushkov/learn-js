025fb906578f99e870fed8a53054706ef6e70148
stas-yushkov <stas.yushkov@gmail.com>
2021-04-09 00:41:17 +0300

    свойство

--- js/script.js
+++ js/script.js
@@ -3,5 +3,11 @@ console.log("hello");
 let b = 2;
 a = b * 2;
 
-console.log(b)
-console.log(a)
+console.log(b);
+console.log(a);
+
+let someString = "sklfdmnvkm15ddq";
+console.log(someString);
+console.log(someString.length)
+
+someString.length
