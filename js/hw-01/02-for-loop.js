c2056dc18abe05eaf7a302a0a1e4f5a142e377d6
stas-yushkov <stas.yushkov@gmail.com>
2021-04-15 02:35:33 +0300

    for

--- js/hw-01/02-for-loop.js
+++ js/hw-01/02-for-loop.js
@@ -18,23 +18,56 @@
 // summ salary
 // console.log
 
-// const,let
-const emploees = 3;
-const min = 500;
-const max = 5000;
-let salary = 0;
-let totalSalary = 0;
+// // const,let
+// const emploees = 3;
+// const min = 500;
+// const max = 5000;
+// let salary = 0;
+// let totalSalary = 0;
 
-// random for  each emploee in for loop
-// summ salary
-for (let i = 0; i < emploees; i++) {
-  console.log(i);
-  salary = Math.round(Math.random() * (max - min) + min);
-  console.log(salary);
-  totalSalary += salary;
+// // random for  each emploee in for loop
+// // summ salary
+// for (let i = 0; i < emploees; i++) {
+//   console.log(i);
+//   salary = Math.round(Math.random() * (max - min) + min);
+//   console.log(salary);
+//   totalSalary += salary;
   
 
-}
-// console.log
-console.log(totalSalary)
+// }
+// // console.log
+// console.log(totalSalary)
+
+
+
+
+
+// Репета:
+// сделать переменные
+// перебрать работников в цикле
+// сгенерить случайное зп
+// прибавить к тоталу
+// лог
+
+// // сделать переменные
+// const minSalary = 500;
+// const maxSalary = 5000;
+// const emploees = 3;
+// let totalSalary = 0;
+// // перебрать работников в цикле
+// for (let i = 1; i <= emploees; i += 1) {
+//   // сгенерить случайное зп
+//   const salary = Math.round(
+//     Math.random() * (maxSalary - minSalary) + minSalary,
+//   )
+//   console.log(`ЗП работника ${i} - ${salary}`);
+//   // прибавить к тоталу
+//   totalSalary += salary;
+// }
+// // лог
+// console.log(totalSalary);
+
+
+
+
 
