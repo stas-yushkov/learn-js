e25eb2145891ba75e3848f996b95743576b5314d
stas-yushkov <stas.yushkov@gmail.com>
2021-04-15 21:30:35 +0300

    ashot

---
+++ js/hw-01/practice/ashot-01.js
@@ -0,0 +1,132 @@
+
+
+// let qq ;
+// console.log(typeof "qdfgvb")
+// console.log(typeof 123)
+// console.log(typeof false)
+// console.log(typeof null)
+// console.log(typeof qq)
+
+
+
+// const apples = 47;
+// const grapes = 135;
+// const result = apples + grapes;
+// console.log(result);
+
+
+
+// let result1 = 108 + 223 - 2 * 5;
+// console.log(result1);
+
+
+
+// let radius = 10;
+// const sqr = Math.PI * Math.pow(radius, 2)
+// console.log(sqr);
+
+// const A = "Ashot";
+// const B = 28;
+// const C = "play football"
+// const message = `hello, my name is ${A}, 
+// I am ${B} years old 
+// and\nI like ${C}`
+// console.log(message);
+
+
+// console.log(5 > 4);//true
+// console.log(10>="7");//true
+// console.log(6>="7");//f
+
+// console.log("2">"12");//t сравнение посимвольно. первый символ 2 ббольше первого символа 12(1) строки преобразованы через коды ASCII
+// console.log("2".charCodeAt(0));//50
+// console.log("1".charCodeAt(0));//49
+// console.log("12".charCodeAt(0));//49
+
+// console.log('2'<'12');//f
+// console.log("4" == 4);//t
+// console.log("6" === 6);//f
+// console.log('false' === false);//f
+// console.log(1 ==true);//t
+// console.log(1 === true);//f
+// console.log('0' == false);//t
+// console.log("0"=== false);//f
+// console.log("Ёнанас" < "ёжик");//t
+// console.log("Ёнанас".charCodeAt(0));//1025
+// console.log("ёжик".charCodeAt(0)); //1105    просто заглавная буква меньше чем строчная по ASCII
+// console.log("Ёнанас" === "ёжик");//f
+// console.log(undefined == null);//t
+// console.log(undefined === null);//f
+
+
+// console.log("н".charCodeAt(0));//1085
+// console.log("ж".charCodeAt(0));//1078
+
+
+// const question = prompt('какое офф название js ?');
+// console.log('ваш ответ', question);
+// if (question === "ECMAScript") {
+//   console.log("верно")
+// } else {
+//   console.log("не знаете? ECMAScript")
+// }
+
+
+
+// const a = 1;
+// const b = 2;
+// let result;
+// // if (a +b < 4) {
+// //   result = "мало";
+// // } else {
+// //   result = "много";
+// // }
+// a + b < 4 ? (result = "мало") : (result = "много");
+// console.log(result);
+
+
+
+// console.log(true && 3 && 5 && true && "rrr");//rrr
+// console.log(true && 3 && null && true && "rrr");//null
+// console.log(false && 3);//false
+// console.log(3 && 4&& 'hello');//hello
+// console.log(true && 0 && "hello");//0
+// console.log(true || 3);//true
+// console.log(true || 3 ||4);//true
+// console.log(true ||false ||7);//true
+// console.log(null || 2 || undefined);//2
+// console.log((1 && null && 2) > 0);//f
+// console.log((1 && null && 2) );//null
+// console.log( null > 0);//f
+// console.log(null || (2 && 3) ||4);//3
+
+
+// написать условие if  для проверки что переменная age находится в диапазоне между 14 и 90 включительно
+// const min = 14;
+// const max = 90;
+// let age = 27;
+// if (age >= min && age <= max) {
+//   console.log('верно');
+// } else {
+//   console.log('лажа');
+// }
+
+
+
+
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+
+
