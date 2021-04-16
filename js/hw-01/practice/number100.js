f4941aeedf99956b3dd7156f413e03fd37bd8369
stas-yushkov <stas.yushkov@gmail.com>
2021-04-16 03:43:53 +0300

    number

--- js/hw-01/practice/number100.js
+++ js/hw-01/practice/number100.js
@@ -6,57 +6,32 @@
 // введёт число, большее 100, либо не нажмёт кнопку
 // Отмена в промпт.
 
-let input = prompt("Введите число больше 100");
+let input;
 let check = false;
 
 do {
-  if (input === null) {
-    console.log('input === null', input)
-    check = true;
-    // input = prompt("Введите число больше 100");
-  } if (input === '') {
+  console.log("Введите число больше 100")
+  input = prompt("Введите число больше 100");
+  
+  if (Number(input) > 100) {
+    console.log('(Number(input) > 100)', Number(input))
+    check = false;
+
+  } else if (input === '') {
     console.log('input is ""', input)
+    check = true;
+
+  } else if (input === null) {
+    console.log('input is null', input)
     check = false;
-    input = prompt("Введите число больше 100");
-  } 
-  else {
-    console.log('input is not null', Number(input))
-    input = prompt("Введите число больше 100");
-    check = false;
+
+  } else if (isNaN(Number(input))) {
+    console.log('input is NaN', input)
+    check = true;
+
+  } else {
+    console.log('input is <= 100', Number(input))
+    check = true;
+    
   }
-
-
-  // if (Number(input) < 100) {
-  //   check = true;
-  //   // input = prompt("Введите число больше 100");
-  // } else {
-  //   check = false;
-  // }
 } while (check);
-
-// добавить проверку на null  и что введенное значение цифра.
-
-
-console.log('Number(input)',Number(input))
-console.log('typeof Number(input) === number',typeof Number(input) === 'number')
-console.log('typeof Number(input) === NaN',typeof Number(input) === typeof Number("input"))
-console.log('input === null',input === null)
-console.log('NaN',NaN )
-// console.log('NaN === NaN', NaN === NaN)
-console.log('isNaN(Number(input))', isNaN(Number(input)))
-
-
-// console.log('typeof 1 === number',typeof 1 === 'number')
-// console.log('null === null',null === null)
-
-// let input = prompt("Введите число больше 100");
-// let check = false;
-
-// do {
-//   if (Number(input) < 100) {
-//     check = true;
-//     // input = prompt("Введите число больше 100");
-//   } else {
-//     check = false;
-//   }
-// } while (check);
