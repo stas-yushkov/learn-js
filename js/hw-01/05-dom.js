3cabd43e08dd827772386ebddfb99c07701fead1
stas-yushkov <stas.yushkov@gmail.com>
2021-04-18 21:30:49 +0300

    addEventListener('click', function

--- js/hw-01/05-dom.js
+++ js/hw-01/05-dom.js
@@ -5,3 +5,7 @@ console.dir(btnAdd.textContent)
 btnAdd.textContent = 'Прибавить'
 
 console.dir(btnAdd.textContent)
+
+btnAdd.addEventListener('click', function () {
+  console.log('click dkwmck;wmdw;cm')
+});
