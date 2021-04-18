ada8522c6be16b27562220cd420d33e1b6bd0967
stas-yushkov <stas.yushkov@gmail.com>
2021-04-18 22:05:17 +0300

    console.log(resetBtn)

--- js/hw-01/05-dom.js
+++ js/hw-01/05-dom.js
@@ -1,14 +1,17 @@
 const btnAdd = document.querySelector('button[data-add]');
+const resetBtn = document.querySelector('button[data-reset]')
 const valueInput = document.querySelector('input[data-value]')
 const outputElement = document.querySelector('.js-output span');
 
+console.log(resetBtn)
+
 let total = 0;
 
-console.dir(btnAdd.textContent);
+// console.dir(btnAdd.textContent);
 
 btnAdd.textContent = 'Прибавить';
 
-console.dir(btnAdd.textContent);
+// console.dir(btnAdd.textContent);
 
 btnAdd.addEventListener('click', function () {
   console.log('click dkwmck;wmdw;cm');
