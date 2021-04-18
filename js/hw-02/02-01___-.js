a05ad2b2791b55660cc67014e901631a6f09766f
stas-yushkov <stas.yushkov@gmail.com>
2021-04-18 23:08:12 +0300

    move....

---
+++ js/hw-02/02-01___-.js
@@ -0,0 +1,14 @@
+// enter in bash 
+// touch 01-{01..36}___-.js
+
+// enter in dev console
+// for (let i = 1; i <= 9; i += 1) {
+//   console.log(`<!-- <script src="./js/hw-01/01-0${i}___-.js" type="module"></script> -->`);
+
+// }
+
+// for (let i = 10; i <= 36; i += 1) {
+//   console.log(`<!-- <script src="./js/hw-01/01-${i}___-.js" type="module"></script> -->`);
+
+// }
+console.log(2)
