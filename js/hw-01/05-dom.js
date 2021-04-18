ce4a94a5a4297f148855338e46c71af12d655eed
stas-yushkov <stas.yushkov@gmail.com>
2021-04-18 21:35:21 +0300

    document.querySelector('button[data-add]');

--- js/hw-01/05-dom.js
+++ js/hw-01/05-dom.js
@@ -1,10 +1,13 @@
-const btnAdd = document.querySelector('button');
+const btnAdd = document.querySelector('button[data-add]');
+const valueInput = document.querySelector('input[data-value]')
 
-console.dir(btnAdd.textContent)
+console.log(valueInput);
 
-btnAdd.textContent = 'Прибавить'
+console.dir(btnAdd.textContent);
 
-console.dir(btnAdd.textContent)
+btnAdd.textContent = 'Прибавить';
+
+console.dir(btnAdd.textContent);
 
 btnAdd.addEventListener('click', function () {
   console.log('click dkwmck;wmdw;cm')
