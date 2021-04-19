bb96ad806fe5193ce3460d481d7f7109957b15ff
stas-yushkov <stas.yushkov@gmail.com>
2021-04-19 03:36:13 +0300

    codewar2

---
+++ js/codewars/002-Return-Negative.js
@@ -0,0 +1,569 @@
+// Kata
+
+// Dashboard
+// Kata
+// Docs
+// Blog
+// Kumite
+// Forum
+// Leaders
+
+// 8 kyu
+// 10
+// 8 kyu
+// Return Negative
+// 42412489% of 10,15033,844 of 91,415Deantwo
+// JavaScript
+// TRAIN AGAINNEXT KATA
+// Details
+// Solutions
+// Forks (9)
+// Discourse (252)
+// Collect|
+// How satisfied are you with this kata?VERYSOMEWHATNONE
+// Show Kata Description
+// Show Kata Test Cases
+// Show Me:
+// All Solutions
+// Solutions of Users I am FollowingMy Solutions
+// Sort By:
+// Best Practices
+// CleverNewestOldest
+// Unihedron, colbydauph, ahitt6345, iawaknahc, NiklasE, rschifini, Twizard, thehobocoder, Worst Mike NA, codeSavvy69 (plus 2371 more warriors)
+// function makeNegative(num) {
+//   return -Math.abs(num);
+// }
+// 300 similar code variations are grouped with this oneShow Variations
+
+// Best Practices470Clever760
+// 28ForkCompare with your solutionLink
+// keune, wthit56, Balkoth, astashuk, Merlin99, tikitariki, liguorui, ViktorSoroka, sushant12, evil_genius (plus 1494 more warriors)
+// function makeNegative(num) {
+//   return num < 0 ? num : -num;
+// }
+// 350 similar code variations are grouped with this oneShow Variations
+
+// Best Practices337Clever143
+// 10ForkCompare with your solutionLink
+// lolmaus, Abdelwahab Bounasser, MichaelSel, chainsaw-maniac, cag, tomerv2, maxfake2, Moropl, golfr, wassim.khleyfi (plus 2 more warriors)
+// makeNegative = n => -Math.abs(n)
+// 4 similar code variations are grouped with this oneShow Variations
+
+// Best Practices44Clever36
+// 1ForkCompare with your solutionLink
+// krillgar, brycehill, davechenww, myTerminal, NotCricket, user4019457, IvanShymanouski, wmay91, DivyanshBatham, melihovv (plus 2046 more warriors)
+// function makeNegative(num) {
+//   return num > 0 ? -num : num;
+// }
+// 405 similar code variations are grouped with this oneShow Variations
+
+// Best Practices38Clever17
+// 0ForkCompare with your solutionLink
+// jessbailes, mAlishera, stevedorke, user9852426, moret_take2, anthkris, alexander_tur, jonchius, bllnd, wildsteelwind (plus 1481 more warriors)
+// function makeNegative(num) {
+//   if ( num > 0 ) {
+//         return -num;
+//     }
+//     else {
+//         return num;
+//     }
+// }
+// 1158 similar code variations are grouped with this oneShow Variations
+
+// Best Practices34Clever10
+// 0ForkCompare with your solutionLink
+// Rynaro, butane, indr, mgocobachi, luis3m, TomMcCluskey, smulligan4930, OlehFirsov, Zeratul, clydealegro (plus 276 more warriors)
+// function makeNegative(num) {
+//   return num <= 0 ? num : num*-1;
+// }
+// 134 similar code variations are grouped with this oneShow Variations
+
+// Best Practices16Clever8
+// 0ForkCompare with your solutionLink
+// Shadow_Cat, rhamilton2k, CodeDraken, Mahmoud_Hashad, Tommydreamer57, Lucky Luke, nbootu, 0x04, slai, FaradayLab (plus 1 more warriors)
+// const makeNegative = n => n<=0?n:-n
+// 6 similar code variations are grouped with this oneShow Variations
+
+// Best Practices9Clever2
+// 0ForkCompare with your solutionLink
+// macocha, jedrekkostecki, austinpray, duopixel, Stamy, elpete, MichelCarroll, Buck, panther7, matthewfarlymn (plus 532 more warriors)
+// function makeNegative(num) {
+//   return Math.abs(num) * -1;
+// }
+// 118 similar code variations are grouped with this oneShow Variations
+
+// Best Practices7Clever8
+// 0ForkCompare with your solutionLink
+// Atokko, SydPorter, VolkRiot, KonradGnat, t3tr4x, khmala777, kung8, pritchardsmith, Antonio47, polor1987 (plus 2 more warriors)
+// function makeNegative(num) {
+//   if(num <= 0){
+//     return num;
+//   }
+//   else
+//      return num *=-1
+// }
+// 10 similar code variations are grouped with this oneShow Variations
+
+// Best Practices4Clever6
+// 0ForkCompare with your solutionLink
+// cedricmckinnie, ruoqian, austinc, Idyllize, mindofmicah, Pemdas, lianbogo, WmBlankenship, dig, iejerry (plus 55 more warriors)
+// function makeNegative(num) {
+//   return 0-Math.abs(num);
+// }
+// 19 similar code variations are grouped with this oneShow Variations
+
+// Best Practices3Clever16
+// 0ForkCompare with your solutionLink
+// Azuaron, yur_ka, Revan88
+// function makeNegative(num) {
+//   return num < 1 ? num : -1 * num;
+// }
+// 1 similar code variation is grouped with this oneShow Variations
+
+// Best Practices3Clever3
+// 4ForkCompare with your solutionLink
+// R-470, bashar_nat, ginnk, imdiv, zus0629, MoscowNights, Scottmcchiu, limabean92, Teddygallion, Shapo (plus 9 more warriors)
+// function makeNegative(num) {
+//   if(num>0){
+//   return -num;
+//   } else if(num<0) {
+//   return num;
+//   } else if(num===0){
+//   return 0;
+//   }
+// }
+// 18 similar code variations are grouped with this oneShow Variations
+
+// Best Practices3Clever0
+// 0ForkCompare with your solutionLink
+// wKich, chrisaw, dstodolny, Hurtak, shaleynikov, warborn, damirkust, jaymie_t, TomZer0, Tawe (plus 148 more warriors)
+// const makeNegative = (num) => -Math.abs(num)
+// 31 similar code variations are grouped with this oneShow Variations
+
+// Best Practices2Clever0
+// 0ForkCompare with your solutionLink
+// JDiddy8250
+// var makeNegative=n=>~(n>>31<<1)*n
+// Best Practices1Clever22
+// 1ForkCompare with your solutionLink
+// nzolotko, Matjulski, Lauriy, maximpact, testr00t, user2355613, igorify, NaMe613, harrigee, pyramidal-neuron (plus 396 more warriors)
+// function makeNegative(num) {
+//   // Code?
+//   if(num > 0) {
+//     return -num;
+//   }
+//   return num;
+// }
+// 273 similar code variations are grouped with this oneShow Variations
+
+// Best Practices1Clever2
+// 0ForkCompare with your solutionLink
+// bartschu
+// function makeNegative(num) {
+//   var number = Number(num<=0 ? num : '-'+num);
+//   return number;
+// }
+// Best Practices1Clever1
+// 0ForkCompare with your solutionLink
+// Clarity, JehandadK, echo419r, ChristofDellhingre, ooflorent, jrandm, rush, amalykhin, Scoutski, hdrdavies (plus 14 more warriors)
+// function makeNegative(num) {
+//   return num<1?num:-num; 
+// }
+// 23 similar code variations are grouped with this oneShow Variations
+
+// Best Practices1Clever1
+// 2ForkCompare with your solutionLink
+// LaPy
+// function makeNegative(num) { return num > 0 ? -num : num  } //dupa
+// Best Practices1Clever0
+// 0ForkCompare with your solutionLink
+// user8169331
+// const makeNegative = (num) => num < 0 ? num : num > 0 ? -Math.abs(num): 0;
+// Best Practices1Clever0
+// 0ForkCompare with your solutionLink
+// irbekrm, DBFritz, bclaudios
+// const makeNegative=x=>x>0?-x:x
+// 2 similar code variations are grouped with this oneShow Variations
+
+// Best Practices1Clever0
+// 0ForkCompare with your solutionLink
+// Mariusz.Dabrowski, Fozzie, Izzur, hector4213, KRAK94, RoqueSanJuan, Jurr, skiapo
+// makeNegative = num => num < 0 ? num : -num
+// 5 similar code variations are grouped with this oneShow Variations
+
+// Best Practices1Clever0
+// 0ForkCompare with your solutionLink
+// kolohelios, EyeCode, benbrowning, karbica, charles.kline, dmytronazarko, chefBoiRLee, Prick, msturdy, ivanhnavi (plus 611 more warriors)
+// function makeNegative(num) {
+//   return num < 0 ? num : num * -1;
+// }
+// 238 similar code variations are grouped with this oneShow Variations
+
+// Best Practices1Clever0
+// 0ForkCompare with your solutionLink
+// misuburlacu, gogromat, Ravetracer, mortiy, maxim-starodubtsev, brogers, chrizzzer, joedriscoll79, jabbson, teemualap (plus 606 more warriors)
+// function makeNegative(num) {
+//   return num <= 0 ? num: -num
+// }
+// 186 similar code variations are grouped with this oneShow Variations
+
+// Best Practices1Clever0
+// 0ForkCompare with your solutionLink
+// deni2020
+// makeNegative  =ㅤ=>ㅤ
+// .constructor(ㅤ
+// .constructor(ㅤ
+// .toString
+// (  +  !!ㅤ+  [  +  !ㅤ+  !ㅤ]  )
+// .constructor
+// .fromCharCode
+// (  [  !!ㅤ+  !!ㅤ+  !!ㅤ+  !!ㅤ]  +  [  !!ㅤ+  !!ㅤ+  !!ㅤ+  !!ㅤ+  !!ㅤ]  )  +ㅤ)
+// .toString
+// (  +  !!ㅤ+  [  +  !ㅤ+  !ㅤ]  )
+// .replace
+// (  +ㅤ[ㅤ]  ,ㅤ)  )
+// ;
+// Best Practices0Clever4
+// 0ForkCompare with your solutionLink
+// wiemannbj27
+// function makeNegative(num) {
+//   if (num.toString().charAt(0) !== "-" && num !== 0) {
+//     return (num*-1);
+//   }
+//   return num; 
+// }
+// Best Practices0Clever3
+// 0ForkCompare with your solutionLink
+// L4mpshade, Trick, Kandros, cijoyah, catco_, coolkingkhan, horsell, Cardbeat, alexwinn, jkcox (plus 147 more warriors)
+// function makeNegative(num) {
+//  if(num > 0){
+//  return num - num * 2
+//  }
+//  else{
+//  return num
+//  }
+// }
+// 147 similar code variations are grouped with this oneShow Variations
+
+// Best Practices0Clever3
+// 0ForkCompare with your solutionLink
+// Maxador, Grae-Drake, CameronEarl, lawrence_nt, maxgwix
+// function makeNegative(num) {
+//   return Math.min(num, num * -1);
+// }
+// 1 similar code variation is grouped with this oneShow Variations
+
+// Best Practices0Clever3
+// 1ForkCompare with your solutionLink
+// wizyoua
+// function makeNegative(num) {
+//   // Code?
+//   if (num >= 0 ) {
+//     var Test = num * -1;
+//     return Test;
+//   } else {
+//     return num;
+//   }
+
+// }
+// Best Practices0Clever2
+// 0ForkCompare with your solutionLink
+// ianwhitedeveloper
+// function makeNegative(num) {
+//   if(num < 0) {
+//     return num;
+//   } else if (num === 0) {
+//     return 0;
+//   } else {
+//     return +('-' + num);
+//   }
+// }
+// Best Practices0Clever2
+// 0ForkCompare with your solutionLink
+// erollroguel
+// function makeNegative(num) {
+//   // Code?
+//   var answ = num - (num + num)
+//   if(num < 1){
+//   return num}
+//   else{
+//   return answ
+//   }
+  
+// }
+// Best Practices0Clever2
+// 1ForkCompare with your solutionLink
+// Remi, gisellegk, wlosiu, needmoreswagger, JavaDoc, ievolved, bombitu, matt167698, aleksey_rabiner, JPMase (plus 88 more warriors)
+// function makeNegative(num) {
+//   if(num > 0){
+//     return 0 - num;
+//   }
+//   else {
+//     return num;
+//   }
+// }
+// 92 similar code variations are grouped with this oneShow Variations
+
+// Best Practices0Clever2
+// 0ForkCompare with your solutionLink
+// btrevathan
+// function makeNegative(num) {
+//   if (num === 0) {
+//     return 0;
+//   } else if (num > 0) {
+//     num = num * -1
+//     return num
+//   } else {
+//     return num
+//   }
+// }
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// IevgenT
+// const makeNegative = num => ~Math.sign(num) && -num || num
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// ShibaCode
+// function makeNegative(num) {
+//   return num.toString().startsWith("-") ? num : (0-num);
+// }
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// askerovtamerlan
+// // function makeNegative(num) {
+// //   // Code?
+// // }
+
+// const makeNegative = num => num > 0 ? -num : num < 0 ? num : 0;
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// pdbusch
+// function makeNegative(num) {
+//   if (num <= 0) {
+//     return num;
+//   } if (num > 0) {
+//     return num * -1;
+//   }
+//   return num;
+// }
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// arlo-sj3
+//  function makeNegative(num) {
+//   if (num > 0){
+//     return -num
+//     }else if (num < 0){
+//       return num
+//     }
+//     else {
+//     return 0
+//     }
+// }
+// makeNegative(-3)
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// srigiriharish
+// function makeNegative(num) {
+//   var result;
+//   //Check if the number is negative or not 
+//   if(num<0)
+//   {
+//   //If it is negative number leave it as it is
+//     result = num;
+//   }
+  
+//   else{
+//   //If the number is positive double it and substract the number with the doubled value to get its negative value
+//   result = num+num;
+//   num = num - result;
+//   result = num;
+//   }
+//   return result;
+// }
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// kvsm, matthewhaworth, KaraM, danmandel, SandQueen, N59F0A66, user145374, user8683675, qquz, Aloha_iloha, 2PR (plus 38 more warriors)
+// const makeNegative = (num) => num <= 0 ? num : -num
+// 21 similar code variations are grouped with this oneShow Variations
+
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// hencethus
+// const makeNegative = (num) => num >>> 31 ? num : ~num + 1;
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// casammar
+// function makeNegative(num) {
+//   var res = num;
+  
+//   if(res > 0)
+//   return res-res-res;
+//   else
+//   return res;
+  
+// }
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// logingit, sumsar181292, Kracy750, AshHolgate, justXna, vledoc33, Hellnar, DavidHJR97, oli92, MoodyBones (plus 5 more warriors)
+// function makeNegative(num) {
+//   if(num <= 0) {
+//       return num;
+//   }
+//   return num - (num * 2);
+// }
+// 14 similar code variations are grouped with this oneShow Variations
+
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// brunolm, Vl4dimyr, alexgertz, vkurchatkin, Zhaggy, wgrisa, aoave, JohanWiltink, mythknight, igor_rajnjak (plus 43 more warriors)
+// const makeNegative = (n) => -Math.abs(n);
+// 15 similar code variations are grouped with this oneShow Variations
+
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// stefannew, rhojn, codemcu, jdns92, siergiej, OleksandrBuchek, Livy, nikolettmlnr, chukaBgd, cburdick50 (plus 27 more warriors)
+// function makeNegative (num) {
+//   return num > 0 ? -Math.abs(num) : num;
+// }
+// 24 similar code variations are grouped with this oneShow Variations
+
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// Code2Empower, keslo, TilinC, _Blank, willgorham, PanDemographic, tyler-morrison, Murilo, user9626075, rmushtaq (plus 122 more warriors)
+// function makeNegative(num) {
+//   if (num > 0){
+//   num *= -1 }
+//   return num;
+// }
+// 102 similar code variations are grouped with this oneShow Variations
+
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// mrfox, bangelakos, awparks, KrisB1022, wardmin, Ulf Nordson, Rastalamm, Brantacus, andrewhmorrison93, gregghanano (plus 437 more warriors)
+// function makeNegative(num) {
+//   if (num <= 0) {
+//     return num }
+//   else {return num*(-1)}
+// }
+// 390 similar code variations are grouped with this oneShow Variations
+
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// wardymate, kennekro, jkron87, crbruns, dschlauderaff, mak17, 72OTA, kapuljaca
+// function makeNegative(num) {
+//  if(num < 1) {
+//    return num;
+//  } else {
+//    return num*-1;
+//  }
+// }
+// 7 similar code variations are grouped with this oneShow Variations
+
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// Mick112165
+// function makeNegative(num){
+// if(num<0){
+//   return num;
+//   } if (num===0){
+//   return num;
+//   } else {return negative = num - num - num}
+ 
+// }
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// Rchitect, BosEriko, Heliux, Delvezio, daishi424, yerim25, Thiago777, dylanjosling@gmail.com, piotrek2108, paheallover (plus 8 more warriors)
+// function makeNegative(num) {
+//   if(num >= 0){
+//     num = num * -1
+//   }
+//     return num;
+// }
+// 17 similar code variations are grouped with this oneShow Variations
+
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// zPat, jrzimmerman, gaguevaras, foleyatwork, jd-gray, amilvasishtha, dim81, tormod17, samisbond, anewandrew (plus 1286 more warriors)
+// function makeNegative(num) {
+//   if(num > 0){
+//     return num * -1;
+//   }else {
+//     return num;
+//   }
+// }
+// 1039 similar code variations are grouped with this oneShow Variations
+
+// Best Practices0Clever1
+// 0ForkCompare with your solutionLink
+// Loading more solutions...
+// © 2021 CodewarsAboutAPIBlogPrivacyTermsContact
+// powered by
+
+
+// Kata
+
+// Dashboard
+// Kata
+// Docs
+// Blog
+// Kumite
+// Forum
+// Leaders
+
+// 8 kyu
+// 7
+// 8 kyu
+// Return Negative
+// 42412489% of 10,15033,843 of 91,414Deantwo
+// JavaScript
+// Node v8.1.3
+// VIM
+// EMACS
+// Instructions
+// Output
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Example:
+
+// makeNegative(1); // return -1
+// makeNegative(-5); // return -5
+// makeNegative(0); // return 0
+// makeNegative(0.12); // return -0.12
+// Notes:
+
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+// FUNDAMENTALSNUMBERS
+
+// powered by
+
+// Solution:
+// 1
+// function makeNegative(num) {
+// 2
+//   // Code?
+// 3
+// }
+// Sample Tests:
+// 1
+// Test.assertEquals(makeNegative(42), -42);
+// SKIP
+// UNLOCK SOLUTIONS
+// DISCUSS (71)
+// RESET
+// TEST
+// ATTEMPT
+
+function makeNegative(num) {
+  if (num > 0) {
+    return -num;
+  } else {
+    return num;
+  }
+}
+
+
+console.log(`makeNegative(1); // return -1`, makeNegative(1));
+console.log(`makeNegative(-5); // return -5`, makeNegative(-5));
+console.log(`makeNegative(0); // return 0`, makeNegative(0));
+console.log(`makeNegative(0.12); // return -0.12`, makeNegative(0.12));
