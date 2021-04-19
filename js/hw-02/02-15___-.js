d621fe9dfb1e29a8250e084eb3788222d421ff35
stas-yushkov <stas.yushkov@gmail.com>
2021-04-19 04:31:39 +0300

    15

--- js/hw-02/02-15___-.js
+++ js/hw-02/02-15___-.js
@@ -56,3 +56,7 @@
 
 // const allClients = ; // Дополни эту строку
 
+const oldClients = ['Манго', 'Аякс', 'Поли', 'Киви'];
+const newClients = ['Персик', 'Хьюстон'];
+
+const allClients = oldClients.concat(newClients); // Дополни эту строку
