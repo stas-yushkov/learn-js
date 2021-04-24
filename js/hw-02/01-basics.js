deddc4539941b5a7a2291fbd52154d22ec4fa2f2
stas-yushkov <stas.yushkov@gmail.com>
2021-04-25 01:24:08 +0300

    function

--- js/hw-02/01-basics.js
+++ js/hw-02/01-basics.js
@@ -1,62 +1,79 @@
-/*
- * Знакомство  с массивами
- * - Объявление
- * - Индексация
- * - Длина
- * - Индекс последнего элемента
- * - Переопределение
- *
- * 'Mango', 'Kiwi', 'Poly', 'Ajax'
- */
+// /*
+//  * Знакомство  с массивами
+//  * - Объявление
+//  * - Индексация
+//  * - Длина
+//  * - Индекс последнего элемента
+//  * - Переопределение
+//  *
+//  * 'Mango', 'Kiwi', 'Poly', 'Ajax'
+//  */
+
+// // const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+// // console.table(friends);
+
+// // friends[1] = 'qwerqrwr';
+// // friends[3] = 123123;
+
+// // console.table(friends);
+
+// // const lastIndex = friends.length - 1;
+// // console.log(lastIndex);
+
+// /*
+//  * Передача по ссылке и по значению
+//  * - Примитивы и сложные типы
+//  * - Ссылочное равенство (referential equality)
+//  */
+
+// // const a = [1, 2, 3];
+// // const b = a;
+
+// // console.log('a', a);
+// // console.log('b', b);
+
+// // a[0] = 500;
+
+// // console.log('a', a);
+// // console.log('b', b);
+
+// // console.log(a === b);
+
+// // console.log([1, 2, 3] === [1, 2, 3]);
+
+// /*
+//  * Перебор (итерация) массива
+//  * - for - если нужен индекс или нужно изменить элемент массива
+//  * - for...of - если индекс не нужен и в массиве ничего менять не нужно
+//  */
 
 // const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
-// console.table(friends);
+// // console.table(friends);
+// // const lastIndex = friends.length - 1;
 
-// friends[1] = 'qwerqrwr';
-// friends[3] = 123123;
-
-// console.table(friends);
-
-// const lastIndex = friends.length - 1;
-// console.log(lastIndex);
-
-/*
- * Передача по ссылке и по значению
- * - Примитивы и сложные типы
- * - Ссылочное равенство (referential equality)
- */
-
-// const a = [1, 2, 3];
-// const b = a;
-
-// console.log('a', a);
-// console.log('b', b);
-
-// a[0] = 500;
-
-// console.log('a', a);
-// console.log('b', b);
-
-// console.log(a === b);
-
-// console.log([1, 2, 3] === [1, 2, 3]);
-
-/*
- * Перебор (итерация) массива
- * - for - если нужен индекс или нужно изменить элемент массива
- * - for...of - если индекс не нужен и в массиве ничего менять не нужно
- */
-
-const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
-// console.table(friends);
-// const lastIndex = friends.length - 1;
-
-for (let i = 0; i < friends.length; i += 1) {
-  friends[i] += `-${i}`;
-}
-
-// for (let friend of friends) {
-//   console.log(friend);
+// for (let i = 0; i < friends.length; i += 1) {
+//   friends[i] += `-${i}`;
 // }
 
-console.table(friends);
+// // for (let friend of friends) {
+// //   console.log(friend);
+// // }
+
+// console.table(friends);
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+const add = function (param_x, param_y) {
+  //....
+  console.log(param_x)
+  console.log(param_y)
+  console.log('function "add" is executing')
+}
+
+
+
+const arg_x = 1;
+const arg_y = 2;
+
+add(arg_x,arg_y);
