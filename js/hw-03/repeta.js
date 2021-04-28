8ac42562853594ad7c33bb37a2f12507f32f3025
stas-yushkov <stas.yushkov@gmail.com>
2021-04-28 19:57:10 +0300

    27k962 TODO: дописать

--- js/hw-03/repeta.js
+++ js/hw-03/repeta.js
@@ -6,11 +6,35 @@
 
 // console.log(playlist[propName]);
 
-const users = ['mango', 'poly'];
+// const users = ['mango', 'poly'];
 
-console.table(users);
-console.log(users.length);
+// console.table(users);
+// console.log(users.length);
 
-users.push('ajax');
-console.table(users);
-console.log(users.length);
+// users.push('ajax');
+// console.table(users);
+// console.log(users.length);
+///////////////////////////////////////////////////////////////////////////////////////////
+// MOCK - тестовые данные (faker, chance)
+// https://cdnjs.com/libraries/Faker
+// https://dev.to/iainfreestone/20-resources-for-generating-fake-and-mock-data-55g1
+
+// STUB - заглушка
+// console.log(faker.address.country());
+
+// function createUserMock() {
+//   return {
+//     country: faker.address.country,
+//     city: faker.address.city,
+//   };
+// }
+
+// const users = [];
+
+// for (let index = 0; index < 10; index++) {
+//   const user = createUserMock();
+//   users.push(user);
+// }
+
+// console.table(users);//https://prnt.sc/127k962 TODO: дописать
+///////////////////////////////////////////////////////////////////////////////////////////
