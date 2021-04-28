2b47643c89fc4c9a984cd1b5e4a43f445c503660
stas-yushkov <stas.yushkov@gmail.com>
2021-04-28 20:34:17 +0300

    function getModels(cars)

--- js/hw-03/repeta.js
+++ js/hw-03/repeta.js
@@ -42,6 +42,42 @@
 ///////////////////////////////////////////////////////////////////////////////////////////
 // уметь в декомпозицию задачи
 ///////////////////////////////////////////////////////////////////////////////////////////
+const vehicles = [
+  { make: 'Honda', model: 'CR-V', type: 'suv', price: 24045 },
+  { make: 'Honda', model: 'Accord', type: 'sedan', price: 22455 },
+  { make: 'Mazda', model: 'Mazda 6', type: 'sedan', price: 24195 },
+  { make: 'Mazda', model: 'CX-9', type: 'suv', price: 31520 },
+  { make: 'Toyota', model: '4Runner', type: 'suv', price: 34210 },
+  { make: 'Toyota', model: 'Sequoia', type: 'suv', price: 45560 },
+  { make: 'Toyota', model: 'Tacoma', type: 'truck', price: 24320 },
+  { make: 'Ford', model: 'F-150', type: 'truck', price: 27110 },
+  { make: 'Ford', model: 'Fusion', type: 'sedan', price: 22120 },
+  { make: 'Ford', model: 'Explorer', type: 'suv', price: 31660 },
+];
+console.table(vehicles);
+
+// const models = [];
+
+// for (const vehicle of vehicles) {
+//   models.push(vehicle.model);
+// }
+
+// console.log(models);
+
+// console.log(Object.keys(vehicles[0]));
+
+function getModels(cars) {
+  // vehicles// недоступна допустим
+  // console.table(cars);
+  const models = [];
+
+  for (const car of cars) {
+    models.push(car.model);
+  }
+  return models;
+}
+
+console.log(getModels(vehicles));
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 
