f45a6238c71113e17423639245f6653c8d2b4884
stas-yushkov <stas.yushkov@gmail.com>
2021-05-03 17:17:39 +0300

    3.33

--- js/hw-03/03-26___-.js
+++ js/hw-03/03-26___-.js
@@ -105,3 +105,45 @@
 // 2020
 // |
 // Политика конфиденциальности
+
+// // Пиши код ниже этой строки
+// function calculateMeanTemperature({
+//   today: { low: todayLow = 10, high: todayHigh = 20 },
+//   tomorrow: { low: tomorrowLow = 20, high: tomorrowHigh = 30 },
+// }) {
+//   // const todayLow = forecast.today.low;
+//   // const todayHigh = forecast.today.high;
+//   // const tomorrowLow = forecast.tomorrow.low;
+//   // const tomorrowHigh = forecast.tomorrow.high;
+
+//   // Пиши код выше этой строки
+//   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+// }
+
+// const forecast = {
+//   today: { low: 10, high: 20 },
+//   tomorrow: { low: 20, high: 30 },
+// };
+
+// console.log(calculateMeanTemperature(forecast)); //20
+
+// // Замени объявления переменных todayLow, todayHigh, tomorrowLow и tomorrowHigh одной операцией деструктуризации свойств объекта forecast.
+
+// // Пиши код ниже этой строки
+// function calculateMeanTemperature({
+//   today: { low: todayLow, high: todayHigh },
+//   tomorrow: { low: tomorrowLow, high: tomorrowHigh },
+// }) {
+//   // Пиши код выше этой строки
+//   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+// }
+
+// Пиши код ниже этой строки
+function calculateMeanTemperature(forecast) {
+  const {
+    today: { low: todayLow, high: todayHigh },
+    tomorrow: { low: tomorrowLow, high: tomorrowHigh },
+  } = forecast;
+  // Пиши код выше этой строки
+  return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+}
