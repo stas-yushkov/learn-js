47dacdc49e5378a5fcd739d384c0263d93ed6be8
stas-yushkov <stas.yushkov@gmail.com>
2021-05-13 12:52:41 +0300

    added initial files

---
+++ js/hw-05/05-00.js
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////////////////////////////////
+// // https://askubuntu.com/questions/605558/how-to-create-multiple-files-with-the-terminal
+// // enter in bash
+// touch 05-{01..19}___-.js
+////////////////////////////////////////////////////////////////////////////
+const moduleNumber = 5;
+const amountOfHW = '19';
+
+console.log(`module ${moduleNumber}`);
+const btnGenerate = document.querySelector('button[data-generate]');
+const valueInputStart = document.querySelector('input[data-start-value]');
+const valueInputEnd = document.querySelector('input[data-end-value]');
+const outputElement = document.querySelector('p[data-text]');
+const sampleCode = document.querySelector('code[data-code]');
+// https://www.w3schools.com/js/js_htmldom_nodes.asp
+
+sampleCode.textContent = `<!-- <script src="./js/hw-0${moduleNumber}/0${moduleNumber}-01___-.js" type="module"></script> -->`;
+btnGenerate.textContent = 'Сгенерировать';
+valueInputStart.defaultValue = '1';
+valueInputEnd.defaultValue = amountOfHW;
+
+btnGenerate.addEventListener('click', function () {
+  console.log('click произошел');
+  const valueStart = Number(valueInputStart.value);
+  const valueEnd = Number(valueInputEnd.value);
+
+  if (valueStart > 0) {
+    for (let i = valueStart; i <= valueEnd; i += 1) {
+      console.log(
+        `<!-- <script src="./js/hw-0${moduleNumber}/0${moduleNumber}-${String(
+          i,
+        ).padStart(2, 0)}___-.js" type="module"></script> -->\n`,
+      );
+
+      const para = document.createElement('p');
+      const node = document.createTextNode(
+        `<!-- <script src="./js/hw-0${moduleNumber}/0${moduleNumber}-${String(
+          i,
+        ).padStart(2, 0)}___-.js" type="module"></script> -->`,
+      );
+
+      para.appendChild(node);
+      const element = document.querySelector('section[data-section]');
+
+      element.appendChild(para);
+    }
+  }
+});
---
+++ js/hw-05/05-pr.js
@@ -0,0 +1,297 @@
+// // // // // // // const qwe = { someKey: 'qwe' };
+
+// // // // // // // console.log(qwe);
+// // // // // // ////////////////////////////////////////////////////////////////
+// // // // // // const user = {
+// // // // // //   name: 'John',
+// // // // // //   age: 20,
+// // // // // //   hobby: 'tennis',
+// // // // // //   premium: true,
+// // // // // // };
+
+// // // // // // // user.mood = 'asdsa';
+// // // // // // // user['mood'] = 'hdfr';
+
+// // // // // // function changeUser(obj) {
+// // // // // //   obj.mood = 'happy';
+// // // // // //   obj.hobby = 'skydiving';
+// // // // // //   obj.premium = false;
+
+// // // // // //   for (const key of Object.keys(obj)) {
+// // // // // //     console.log(`${key}: ${obj[key]}`);
+// // // // // //   }
+
+// // // // // //   return obj;
+// // // // // // }
+
+// // // // // // console.log(changeUser(user));
+
+// // // // // // ////////////////////////////////////////////////////////////////
+// // // // // const salaries = {
+// // // // //   John: 100,
+// // // // //   Ann: 160,
+// // // // //   Pete: 130,
+// // // // // };
+
+// // // // // // console.log(typeof null);
+
+// // // // // function sumSalaries(objSalaries) {
+// // // // //   if (!objSalaries) return 0; //если обьект пуст, тоесть null, вернуть ноль
+// // // // //   let sum = 0;
+
+// // // // //   // const objSalaries = {};
+
+// // // // //   // console.log({}); //{}
+// // // // //   // console.log(typeof {}); //object
+// // // // //   // console.log(Boolean({})); //true
+// // // // //   // console.log(objSalaries); //{John: 100, Ann: 160, Pete: 130}
+// // // // //   // console.log(!objSalaries); //false
+
+// // // // //   for (const value of Object.values(objSalaries)) {
+// // // // //     // console.log(value);
+// // // // //     sum += value;
+// // // // //   }
+
+// // // // //   return sum;
+// // // // // }
+
+// // // // // console.log(sumSalaries(salaries));
+
+// // // // // // ////////////////////////////////////////////////////////////////
+// // // // const stones = [
+// // // //   { name: 'Изумруд', price: 1300, quantity: 4 },
+// // // //   { name: 'Бриллиант', price: 2700, quantity: 3 },
+// // // //   { name: 'Сапфир', price: 400, quantity: 7 },
+// // // //   { name: 'Щебень', price: 200, quantity: 2 },
+// // // // ];
+
+// // // // function calcTotalPrice(stones, stoneName) {
+// // // //   let sum = 0;
+// // // //   for (const stone of stones) {
+// // // //     if (stone['name'] === stoneName) {
+// // // //       sum = stone['price'] * stone['quantity'];
+// // // //     }
+// // // //   }
+// // // //   return sum;
+// // // // }
+
+// // // // console.log(calcTotalPrice(stones, 'Щебень'));
+
+// // // // // // ////////////////////////////////////////////////////////////////
+
+// // // const calculator = {
+// // //   read(a, b) {
+// // //     this.value1 = a;
+// // //     this.value2 = b;
+// // //   },
+// // //   sum() {
+// // //     return !this.value1 || this.value2 ? false : this.value1 + this.value2; // TODO: переписать проверку на &&
+// // //   },
+// // //   mult() {
+// // //     return !this.value1 || this.value2 ? false : this.value1 * this.value2;
+// // //   },
+// // // };
+
+// // // // console.log(calculator);
+// // // console.log(calculator.sum());
+// // // console.log(calculator.read(2, 3));
+// // // console.log(calculator);
+// // // console.log(calculator.sum());
+// // // console.log(calculator.mult());
+// // // console.log(undefined + 1); //NaN
+
+// // // // // // ////////////////////////////////////////////////////////////////
+
+// // // const updateObj = (obj, removeKey) => {
+// // //   let newObj = { ...obj };
+
+// // //   delete newObj[removeKey];
+
+// // //   return newObj;
+// // // };
+
+// // // console.log(updateObj({ a: 1, b: 2 }, 'b'));
+
+// // // // // // ////////////////////////////////////////////////////////////////
+
+// // const updateObj = (obj, ...removeKeys) => {
+// //   let newObj = { ...obj };
+
+// //   for (const removeKey of removeKeys) {
+// //     delete newObj[removeKey];
+// //   }
+
+// //   return newObj;
+// // };
+
+// // // const objTest = { a: 1, b: 2, c: 3, d: { e: 111 } };
+
+// // // console.log(objTest.d.e);//111
+
+// // // let newTestObj = { ...objTest };
+
+// // // console.log(newTestObj.d.e);//111
+
+// // // newTestObj.d.e = 222;
+
+// // // console.log(objTest.d.e);//222
+// // // console.log(newTestObj.d.e);//222
+// // console.log(updateObj(objTest, 'b', 'a'));
+// // // // // // ////////////////////////////////////////////////////////////////
+// const objData = {
+//   name: 'John',
+//   surName: 'Stones',
+//   age: 25,
+//   hobby: 'football',
+//   merried: false,
+// };
+
+// function changeObj(obj) {
+//   const newObj = Object.entries(obj);
+//   console.log(newObj);
+// }
+
+// changeObj(objData); //TODO: сделать тожесамое но без Object.entries, может через полифил
+// // // // // // ////////////////////////////////////////////////////////////////
+
+// -----------------------
+
+// 7) Напиши скрипт управления личным
+// кабинетом интернет банка.
+// Есть объект account в котором необходимо
+// реализовать методы для работы
+// с балансом и историей транзакций.
+
+/*
+ * Типов транзацкий всего два.
+ * Можно положить либо снять деньги со счета.
+ */
+const Transaction = {
+  DEPOSIT: 'deposit',
+  WITHDRAW: 'withdraw',
+};
+
+let transactionID = 0;
+
+/*
+ * Каждая транзакция это объект со свойствами: id, type и amount
+ */
+
+const account = {
+  // Текущий баланс счета
+  balance: 0,
+
+  // История транзакций
+  transactions: [],
+
+  /*
+   * Метод создает и возвращает объект транзакции.
+   * Принимает сумму и тип транзакции.
+   */
+  createTransaction(type, amount) {
+    return {
+      id: (transactionID += 1),
+      // id: ++transactionID,
+      type,
+      amount,
+    };
+  },
+
+  /*
+   * Метод отвечающий за добавление суммы к балансу.
+   * Принимает сумму транзакции.
+   * Вызывает createTransaction для создания объекта
+   * транзакции
+   * после чего добавляет его в историю транзакций
+   */
+  deposit(amount) {
+    this.balance += amount;
+    const transaction = this.createTransaction(Transaction.DEPOSIT, amount);
+    this.transactions.push(transaction);
+    // this.balance += amount;
+    // const transaction = this.createTransaction(Transaction.DEPOSIT, amount);
+    // this.transactions.push(transaction);
+  },
+
+  /*
+   * Метод отвечающий за снятие суммы с баланса.
+   * Принимает сумму танзакции.
+   * Вызывает createTransaction для создания объекта
+   * транзакции
+   * после чего добавляет его в историю транзакций.
+   *
+   * Если amount больше чем текущий баланс, выводи
+   * сообщение
+   * о том, что снятие такой суммы не возможно,
+   *  недостаточно средств.
+   */
+  withdraw(amount) {
+    if (amount > this.balance) {
+      return 'Cнятие такой суммы не возможно, недостаточно средств';
+    } else {
+      this.balance -= amount;
+      const transaction = this.createTransaction(Transaction.WITHDRAW, amount);
+      this.transactions.push(transaction);
+    }
+  },
+
+  /*
+   * Метод возвращает текущий баланс
+   */
+  getBalance() {
+    return this.balance;
+  },
+
+  /*
+   * Метод ищет и возвращает объект транзации по id
+   */
+  getTransactionDetails(id) {
+    for (const transaction of this.transactions) {
+      if (transaction['id'] === id) {
+        return transaction;
+      }
+    }
+
+    // for (const transaction of this.transactions) {
+    //   if (transaction['id'] === id) {
+    //     return transaction;
+    //   }
+    // }
+  },
+
+  /*
+   * Метод возвращает количество средств
+   * определенного типа транзакции из всей истории
+   * транзакций
+   */
+  getTransactionTotal(type) {
+    let sum = 0;
+
+    for (const transaction of this.transactions) {
+      if (transaction['type'] === type) {
+        sum += transaction['amount'];
+      }
+    }
+
+    return sum;
+  },
+};
+
+account.deposit(3000);
+console.log(account.getTransactionDetails(0));
+account.deposit(3000);
+account.deposit(3000);
+account.deposit(3000);
+
+account.withdraw(1000);
+
+console.log(account.getBalance());
+
+console.log(account.transactions);
+
+console.log(account.getTransactionDetails(1));
+
+console.log(account.getTransactionTotal(Transaction.DEPOSIT));
+
+// // // // // // ////////////////////////////////////////////////////////////////
+// https://prnt.sc/129kttj Ашот фото)
