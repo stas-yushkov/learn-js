7bb26516db251ab59eb26830733ccac1c0e6370c
stas-yushkov <stas.yushkov@gmail.com>
2021-05-13 12:44:00 +0300

    upd

---
+++ js/hw-04/04pr.js
@@ -0,0 +1,41 @@
+// // console.log(4);
+// // // //
+// // // //
+// // // //
+// // // console.log(Date());
+
+// // // console.log(new Date()); //Thu May 06 2021 20:17:11 GMT+0300 (Восточная Европа, летнее время)
+
+// // // Наблюдается последовательность и зависимость параметров в замыкании, если написать все через стрелки:
+// // // const makeShef = shefName => dish => console.log(`${shefName} готовит ${dish}`); TODO: Проверить
+
+// // //
+
+// // const makeCounter = () => {
+// //   let counter = 0;
+// //   return () => (counter += 1);
+// // };
+
+// // const counter = makeCounter();
+
+// // console.log(counter());
+// // console.log(counter());
+// // console.log(counter());
+// // console.log(counter());
+
+// // // ...............................................
+
+// // const savePWD = pwd => {
+// //   return pwd2 => pwd === pwd2;
+// // };
+
+// // const pwd1 = savePWD('aaa');
+
+// // console.log(pwd1('aadjcnkdncka'));
+
+// // console.log(pwd1('aaa'));
+// // // ...............................................
+// console.log(2 ** 2); //4
+
+// console.log(Date.now()); //1620325165095
+// // ...............................................
